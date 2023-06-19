0f6d0e09c0af63609e7b4cec7596ecc87a2b32f2
Dennis Nielsen <62034663+zploited@users.noreply.github.com>
2023-06-19 22:41:39 +0200

    added missing project files

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zploited\Heimdall\Tests;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [];
+    }
+}
