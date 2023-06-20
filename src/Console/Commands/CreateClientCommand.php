a0c738e38da945cce7336bbfdd3f07588cb53d84
Dennis Nielsen <62034663+zploited@users.noreply.github.com>
2023-06-20 16:24:50 +0200

    added redirect_uri to create client command

--- src/Console/Commands/CreateClientCommand.php
+++ src/Console/Commands/CreateClientCommand.php
@@ -3,18 +3,21 @@
 namespace Zploited\Heimdall\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Zploited\Heimdall\Models\Client;
 
 class CreateClientCommand extends Command
 {
     protected $signature = 'client:create 
-                                {name : Display name of the new client.}';
+                                {name : Display name of the new client.}
+                                {--redirect-uri= : Adds a given redirect uri to the client.}';
     protected $description = 'Creates a new client and stores it in the database.';
 
     public function handle(): int
     {
         Client::create([
-            'name' => $this->argument('name')
+            'name' => $this->argument('name'),
+            'redirect_uri' => $this->option('redirect-uri') ? [$this->option('redirect-uri')] : null
         ]);
 
         return 0;
--- tests/Feature/ClientCommandsTest.php
+++ tests/Feature/ClientCommandsTest.php
@@ -16,4 +16,13 @@ class ClientCommandsTest extends TestCase
 
         $this->assertDatabaseCount(Client::class, 1);
     }
+
+    public function test_console_can_add_redirect_uri_while_creating_client(): void
+    {
+        $this->artisan('client:create test-client --redirect-uri=https://mydomain.tld')
+            ->assertOk();
+
+        $this->assertDatabaseCount(Client::class, 1);
+        $this->assertEquals('https://mydomain.tld', Client::first()->redirect_uri->first());
+    }
 }
